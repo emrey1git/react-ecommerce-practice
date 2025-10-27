0c2b842af3f947995f772c43d8877aa5f00c38b9
Emre Yiğit <emreygt01@icloud.com>
2025-10-28 00:41:08 +0300

    header

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import PageContainer from './container/pageContainer';
+import Header from './components/Header';
+
+function App() {
+
+  return (
+    <>
+     <PageContainer>
+      <Header />
+     </PageContainer>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import "../css/Header.css";
+import { LuShoppingBasket } from "react-icons/lu";
+import { FaMoon } from "react-icons/fa";
+import { MdOutlineLightMode } from "react-icons/md";
+
+const Header = () => {
+  const [theme, setTheme] = useState(false);
+  const changeTheme = () => {
+    const root = document.getElementById("root");
+    if (!theme) {
+      root.style.backgroundColor = "black";
+      root.style.color = "#ffff";
+    } else {
+      root.style.backgroundColor = "#ffff";
+      root.style.color = "black";
+    }
+    setTheme(!theme);
+  };
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        padding: "10px 0",
+      }}
+    >
+      <div className="flex-row">
+        <img className="logo" src="./src/images/logo.png" />
+        <p className="logoText">Elenzo</p>
+      </div>
+
+      <div className="flex-row">
+        <input
+          className="searchInput"
+          type="text"
+          placeholder="Bir şeyler arayın..."
+        />
+
+        <div className="header-icons">
+          {theme ? (
+            <FaMoon className="header-icons" onClick={changeTheme} />
+          ) : (
+            <MdOutlineLightMode
+              className="header-icons"
+              onClick={changeTheme}
+            />
+          )}
+
+          <LuShoppingBasket />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
