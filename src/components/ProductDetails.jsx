8aa3a97aa44976af2d2442abd50a28ff56789529
Emre Yiğit <emreygt01@icloud.com>
2025-11-16 15:52:20 +0300

    added

--- src/components/ProductDetails.jsx
+++ src/components/ProductDetails.jsx
@@ -2,12 +2,18 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { setSelectedProduct } from "../redux/slices/productSlice.jsx";
+import { IoIosAddCircleOutline } from "react-icons/io";
+import { IoIosRemoveCircleOutline } from "react-icons/io";
+
+import '../App.css';
+import { BiFontFamily } from "react-icons/bi";
 
 function ProductDetails() {
   const { id } = useParams();
   const { products, selectedProduct } = useSelector((state) => state.products);
+  const { title, image, price, description } = selectedProduct;
   const dispatch = useDispatch();
- 
+
   const getProductById = () => {
     if (products && products.length > 0) {
       const foundProduct = products.find(
@@ -25,16 +31,20 @@ function ProductDetails() {
   }, [id, products, dispatch]);
 
   return (
-    <div>
-      {selectedProduct && selectedProduct.title ? (
-        <div>
-          <h3>{selectedProduct.title}</h3>
-          <p>{selectedProduct.price} ₺</p>
-          <p>{selectedProduct.description}</p>
+    <div  style={{ marginTop: "30px", display: 'flex', flexDirection: 'row', justifyContent: 'center' }}>
+      <div style={{marginRight: '50px'}}>
+        <img src={image} alt="" width={300} height={500} />
+      </div>
+      <div>
+        <h1 style={{fontFamily:'arial'}}>{title}</h1>
+        <p style={{fontFamily:'arial'}}>{description}</p>
+        <h1 style={{fontFamily:'arial', fontSize: '50px' ,fontWeight: 'bold',color: 'red'}}>{price}₺</h1>
+        <div style={{display:'flex',alignItems:'center'}}>
+        <IoIosAddCircleOutline style={{fontSize: '40px', cursor:'pointer'}  }/> <span style={{fontSize:'25px'}}>0</span> <IoIosRemoveCircleOutline style={{fontSize: '40px', cursor:'pointer'}}/> 
+       
         </div>
-      ) : (
-        <p>Ürün detayları yükleniyor...</p>
-      )}
+         <button style={{marginTop:'10px'}}>Sepete Ekle</button>
+      </div>
     </div>
   );
 }
