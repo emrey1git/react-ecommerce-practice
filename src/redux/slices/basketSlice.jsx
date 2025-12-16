92144260eb0ecfaa8d58fb57bf11cd714d1e621a
Emre Yiğit <emreygt01@icloud.com>
2025-12-16 07:58:45 +0300

    delete button fixed

--- src/redux/slices/basketSlice.jsx
+++ src/redux/slices/basketSlice.jsx
@@ -22,27 +22,26 @@ const writeBasketToStorage = (basket) => {
 }
 
 const initialState = {
- 
+    // Sepeti başlangıçta storage'dan yükle
     products: getBasketFromStorage(),
 }
 
 export const basketSlice = createSlice({
     name: 'basket',
     initialState,
+    
     reducers: {
         addToBasket: (state, action) => {
             
-         
             const findProduct = state.products.find(
                 (product) => product.id === action.payload.id
             );
             
             if (findProduct) {
-               
+                // Ürün zaten varsa: Miktarı artır
                 findProduct.quantity += action.payload.quantity || 1; 
             } else { 
-             
-                
+                // Ürün yoksa: Yeni ürünü 'products' array'ine ekle.
                 state.products.push({ 
                     ...action.payload, 
                     quantity: action.payload.quantity || 1 
@@ -50,10 +49,22 @@ export const basketSlice = createSlice({
             }
             
             writeBasketToStorage(state.products);
-        }
-    }, 
+        },
+        
+        removeFromBasket: (state, action) => {
+            const productIdToRemove = action.payload; 
+            
+            // products array'ini filtreleyerek ID'si eşleşmeyenleri tut
+            state.products = state.products.filter(
+                (product) => product.id !== productIdToRemove
+            );
+            
+            // LocalStorage'ı güncelle
+            writeBasketToStorage(state.products); 
+        } 
+    },
 });
 
-export const { addToBasket } = basketSlice.actions
+export const { addToBasket, removeFromBasket } = basketSlice.actions 
 
 export default basketSlice.reducer
